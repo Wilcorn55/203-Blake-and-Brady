25a950983aaac01a5193cdf50e1802afd548ec9a
Wilcorn55 <Blakewilburn@hotmail.com>
2022-05-31 11:25:10 +1200

    style changes

--- CV_Builder/App/views.py
+++ CV_Builder/App/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import HttpResponseNotFound
+
 
 # Create your views here.
