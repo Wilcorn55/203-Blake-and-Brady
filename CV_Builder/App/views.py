f704a984537b9a85101bdbc04dbf68d0a3a8af15
Wilcorn55 <84747115+Wilcorn55@users.noreply.github.com>
2022-06-02 22:52:31 +1200

    completed Register

--- CV_Builder/App/views.py
+++ CV_Builder/App/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import HttpResponseNotFound
+from faunadb import query as q
+import pytz
+from faunadb.objects import Ref
+from faunadb.client import FaunaClient
+import hashlib
+import datetime
 
 
 # Create your views here.
