2764d7c3b2c953e3452324c32011d3b7cf56203d
Wilcorn55 <84747115+Wilcorn55@users.noreply.github.com>
2022-06-03 17:01:30 +1200

    updates to html files

--- CV_Builder/App/views.py
+++ CV_Builder/App/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib import messages
-from django.http import HttpResponseNotFound
+from django.http import HttpResponse
 from faunadb import query as q
 import pytz
 from faunadb.objects import Ref
 from faunadb.client import FaunaClient
 import hashlib
 import datetime
+
+def home(request):
+    return HttpResponse("<h1>Home</h1>")
 
 
 # Create your views here.
--- CV_Builder/CV_Builder/urls.py
+++ CV_Builder/CV_Builder/urls.py
@@ -14,15 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.staticfiles.urls import static
+from . import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("App.urls"))
+    path('', views.home)
 ]
 
 urlpatterns += staticfiles_urlpatterns()
